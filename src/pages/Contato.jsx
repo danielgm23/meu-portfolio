8027f055e2aeec5f4966f22b711194241cf52542
danielgm23 <danielgm.webcontato@gmail.com>
2024-09-14 14:22:14 -0300

    first commit

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Contato() {
+    <>
+    conteudo do contato
+    </>
+}   
+
+export default Contato
---
+++ src/pages/SobreMim.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SobreMim () {
+    <>
+    conteudo sobre mim
+    </>
+}   
+
+export default SobreMim
